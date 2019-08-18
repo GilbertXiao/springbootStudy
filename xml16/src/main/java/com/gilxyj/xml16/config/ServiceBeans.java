a3b689c202e6b1aacc065ad62186911a6bb70c87
GilbertXiao <gilxyj@163.com>
2019-08-18 14:18:47 +0800

    springboot config with xml

---
+++ xml16/src/main/java/com/gilxyj/xml16/config/ServiceBeans.java
@@ -0,0 +1,15 @@
+package com.gilxyj.xml16.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @program: xml16
+ * @description:
+ * @author: GilbertXiao
+ * @create: 2019-08-17 23:18
+ **/
+@Configuration
+@ImportResource("classpath:/beans.xml")
+public class ServiceBeans {
+}
---
+++ xml16/src/main/java/com/gilxyj/xml16/service/HelloService.java
@@ -0,0 +1,15 @@
+package com.gilxyj.xml16.service;
+
+/**
+ * @program: xml16
+ * @description:
+ * @author: GilbertXiao
+ * @create: 2019-08-17 23:05
+ **/
+public class HelloService {
+
+    public String hello(String name){
+        return "hello:"+name;
+    }
+
+}
